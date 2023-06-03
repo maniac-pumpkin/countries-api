f2162b7921f929f2c73d245baf3c4e9d4fd84210
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-06-03 16:57:15 +0330

    Making a gulpFile for scss

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+
+const scssFilesPath = "./styles/**/*.scss";
+
+function buildStyles() {
+	return src(scssFilesPath).pipe(sass().on("error", sass.logError())).pipe(rename("style.css")).pipe(dest("./styles"));
+}
+
+function watchSass() {
+	watch(scssFilesPath, buildStyles);
+}
+
+exports.default = series(buildStyles, watchSass);
