0a77da6792613939744b3f0004edd90444940969
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-06-05 00:28:08 +0330

    Update #2

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 "use strict";
 
-import { renderCards } from "./function.js";
+import { retrieveCards, renderer } from "./function.js";
+
+function getCountryName(name) {
+	const apiURL = `https://restcountries.com/v3.1/name/${name}`;
+	fetch(apiURL)
+		.then((response) => response.json())
+		.then((data) => {
+			renderer(main, data, retrieveCards);
+			console.log(data);
+		});
+}
+
+const main = document.querySelector("#main");
+
+document.querySelector(".nav__search-btn").addEventListener("click", () => {
+	const searchInput = document.querySelector(".nav__search");
+	[...main.children].length && [...main.children].forEach((child) => child.remove());
+	searchInput.value && getCountryName(searchInput.value);
+});
